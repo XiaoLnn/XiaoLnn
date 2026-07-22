8f1e31318e542141c67b6faeb73f2a6805097c3c
XiaoLnn <1652939626@qq.com>
2026-07-22 14:23:45 +0800

    Update music.js

--- feifei/js/music.js
+++ feifei/js/music.js
@@ -1,6 +1,6 @@
-let server = "tencent"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
+let server = "netease"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
 let type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
-let id = "7764944594"; //封面 ID / 单曲 ID / 歌单 ID
+let id = "5023801844"; //封面 ID / 单曲 ID / 歌单 ID
 let ap = null;
 const metingApiBase = "https://kennyz.cn:11444/meting/";
 
