a0f87860b16abc30072b98d3dbbe409dff5a1fe1
XiaoLnn <1652939626@qq.com>
2026-07-30 19:02:55 +0800

    config: connect preview frontend to test worker

--- music/assets/js/config.js
+++ music/assets/js/config.js
@@ -1,7 +1,7 @@
 window.NIE_MUSIC_CONFIG = Object.freeze({
   // 部署 Cloudflare Worker 后改成你的地址，例如：
   // https://nie-music-api.your-account.workers.dev
-  WORKER_BASE_URL: "",
+  WORKER_BASE_URL: "https://nie-music-api-test.1652939626-a36.workers.dev/",
   APP_NAME: "Nie Music",
   DEFAULT_SEARCH_LIMIT: 12
 });
